23bd916e903f5d5630b59d040c9cbde47be69782
bidiboom <bidiboom@gunduz.fr>
2015-02-02 10:31:31 +0000

    update build

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 var gulp = require('gulp');
 var typescript = require('gulp-typescript');
 var sourcemaps = require('gulp-sourcemaps');
+var util = require("gulp-util");
+
 var del = require('del');
 var runSequence = require('run-sequence');
-
+var exec = require("child_process").exec;
+var glob = require("glob");
 
 
 
@@ -69,7 +72,42 @@ gulp.task("test-transpile3", function() {
     .pipe(gulp.dest("compiled/test3"));
 });
 
+gulp.task("test-transpile4", function() {
+   return gulp.src("src/**/*.ts") 
+    .pipe(sourcemaps.init())
+    .pipe(typescript(
+        {
+            sourceRoot:"",
+            target:'ES5'
+        })).js
+    .pipe(sourcemaps.write('./', {
+        includeContent:false
+    }))
+    .pipe(gulp.dest("compiled/test4"));
+});
 
-gulp.task("default", function() {
-    runSequence('clean','test-transpile0','test-transpile1','test-transpile2','test-transpile3');
+gulp.task("test-transpile5", function(callback) {
+    glob("src/**/*.ts",function (err, files) {
+        if (err) {
+            util.log("error : " + err);
+            throw new util.PluginError('build', "unable to handle tranpiling " + err);
+        }
+        exec(
+            'node node_modules/typescript/bin/tsc --outDir compiled/test5 --sourceMap ""'+ files.join(" "), 
+            function (error, stdout, stderr) {
+                if (error) {
+                    throw new util.PluginError('build', 'unable to launch node tsc : ' + error);
+                }
+                
+                util.log("transpiling finished");
+                callback();
+            }
+        );
+        
+    });
+    
+});
+
+gulp.task("default", function(cb) {
+    runSequence('clean','test-transpile0','test-transpile1','test-transpile2','test-transpile3','test-transpile4','test-transpile5',cb);
 });
